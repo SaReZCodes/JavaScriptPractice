964019f76650b48a35c0b520a695021f09c3e104
saeedr22 <saeed.rezaei1@gmail.com>
2021-05-14 18:15:00 +0430

    Create sample02.js

---
+++ This-Practice/sample02.js
@@ -0,0 +1,29 @@
+function write(num) {
+   console.log(num);
+   this.count++;
+}
+write.count=0;
+for(i =0 ;i<10;i++){
+   if(i>5)
+      write(i);
+}
+
+
+console.log(write.count);// 0 
+
+function write(num) {
+	console.log(  num );
+	data.count++;
+}
+
+var data = {
+	count: 0
+};
+
+for (i=0; i<10; i++) {
+	if (i > 5) {
+		write( i );
+	}
+}
+console.log( data.count ); // 4
+
