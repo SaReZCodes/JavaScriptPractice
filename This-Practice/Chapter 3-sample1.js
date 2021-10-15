6897723d40ad5e9719bd9f04a903d98b96126dab
Saeed Rezaei <saeed.rezaei1@gmail.com>
2021-10-15 23:16:44 +0330

    Create Chapter 3-sample1.js

---
+++ This-Practice/Chapter 3-sample1.js
@@ -0,0 +1,15 @@
+//The first method
+var obj1 = {
+  id: 1,
+  name: 'saeed',
+  family: 'rezaei'
+}
+
+//The second method
+var obj2 = new Object();
+obj2.id = 2;
+obj2.name = 'reza';
+obj2.family = 'mohammadi';
+
+console.log(obj1.id, obj1.name, obj1.family);
+console.log(obj2.id, obj2.name, obj2.family);
